92425389cb27a11db37a6bddfdb57b09fbbc06fd
siyual.park <siyual.bak@gmail.com>
2021-03-20 16:33:00 +0900

    fix: build:prd script

--- packages/koa-verify-joi/lib/by-schema.ts
+++ packages/koa-verify-joi/lib/by-schema.ts
@@ -1,6 +1,7 @@
 import { Schema } from "joi";
 import { BadRequest } from "http-errors";
 
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 function bySchema<P = any>(schema: Schema): (value: P) => Promise<void> {
   return async (value) => {
     try {
