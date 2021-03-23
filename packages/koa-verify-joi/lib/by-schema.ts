e6489a236cab147e26ffed2325baf372a87ff205
siyual.park <siyual.bak@gmail.com>
2021-03-23 11:37:55 +0900

    fix: add schema type

--- packages/koa-verify-joi/lib/by-schema.ts
+++ packages/koa-verify-joi/lib/by-schema.ts
@@ -2,7 +2,7 @@ import { Schema } from "joi";
 import { BadRequest } from "http-errors";
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-function bySchema<P = any>(schema: Schema): (value: P) => Promise<void> {
+function bySchema<P = any>(schema: Schema<P>): (value: P) => Promise<void> {
   return async (value) => {
     try {
       await schema.validateAsync(value);
