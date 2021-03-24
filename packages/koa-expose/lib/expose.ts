e40d503195e2e0ab4402d094f8a27870424d43cb
siyual-park <kdpark0723@gmail.com>
2021-03-24 18:51:27 +0900

    refactoring

--- packages/koa-expose/lib/expose.ts
+++ packages/koa-expose/lib/expose.ts
@@ -2,33 +2,41 @@ import Application from "koa";
 import { Extractor, response } from "koa-position";
 import { DeepPartial } from "@course-design/types";
 
-import defaultPick from "./pick";
+import pick from "./pick";
 
 const bodyPosition = response("body");
 
-function expose<T>(
-  extractor: Extractor,
-  pick: (
+export type ExposeOptions<T> = {
+  pick?: (
     value: T,
     fields: string[]
-  ) => DeepPartial<T> | Promise<DeepPartial<T>> = defaultPick
+  ) => DeepPartial<T> | Promise<DeepPartial<T>>;
+};
+
+function expose<T>(
+  extractor: Extractor,
+  options?: ExposeOptions<T>
 ): Application.Middleware {
+  const finalPick = options?.pick ?? pick;
+
   return async (context, next) => {
     const field = await extractor.extract(context);
-    const fields = Array.isArray(field) ? field : [field];
+    if (field != null) {
+      const fields = Array.isArray(field) ? field : [field];
+      const originBody = await bodyPosition.extract(context);
 
-    const originBody = await bodyPosition.extract(context);
-    let result: unknown = context.response.body;
-    if (originBody != null) {
-      if (Array.isArray(originBody)) {
-        result = await Promise.all(
-          originBody.map((value) => pick(value as T, fields))
-        );
-      } else {
-        result = await pick(originBody as T, fields);
+      let result: unknown = originBody;
+      if (originBody != null) {
+        if (Array.isArray(originBody)) {
+          result = await Promise.all(
+            originBody.map((value) => finalPick(value as T, fields))
+          );
+        } else {
+          result = await finalPick(originBody as T, fields);
+        }
       }
+      await bodyPosition.inject(context, result);
     }
-    await bodyPosition.inject(context, result);
 
     await next();
   };
