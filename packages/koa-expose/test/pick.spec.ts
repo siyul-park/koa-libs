6d8b9c7ca41468d92fa8bfc8545f3de38e5d4b89
siyual-park <kdpark0723@gmail.com>
2021-03-24 22:48:08 +0900

    Add more test

--- packages/koa-expose/test/pick.spec.ts
+++ packages/koa-expose/test/pick.spec.ts
@@ -15,12 +15,17 @@ test("pick", () => {
         },
         e: 10,
       },
+      {
+        f: {
+          g: 10,
+        },
+        e: 10,
+      },
     ],
     h: 1,
   };
 
-  const result = pick(obj, ["a.b.c", "e.0.e"]);
-  expect(result).toEqual({
+  expect(pick(obj, ["a.b.c", "e.0.e"])).toEqual({
     a: {
       b: {
         c: 10,
@@ -31,5 +36,42 @@ test("pick", () => {
         e: 10,
       },
     ],
+  });
+
+  expect(pick(obj, ["d", "e"])).toEqual({
+    e: [
+      {
+        f: {
+          g: 10,
+        },
+        e: 10,
+      },
+      {
+        f: {
+          g: 10,
+        },
+        e: 10,
+      },
+    ],
+  });
+
+  expect(pick(obj, ["a.e"])).toEqual({ a: {} });
+
+  expect(pick(obj, ["a", "a.b.c"])).toEqual({
+    a: {
+      b: {
+        c: 10,
+        d: 5,
+      },
+    },
+  });
+
+  expect(pick(obj, ["a.b.c", "a"])).toEqual({
+    a: {
+      b: {
+        c: 10,
+        d: 5,
+      },
+    },
   });
 });
