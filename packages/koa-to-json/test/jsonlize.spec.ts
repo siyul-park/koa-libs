66ac0a8bd0abe97058e85f584b73eec7d5a034f9
siyual-park <kdpark0723@gmail.com>
2021-03-25 15:28:56 +0900

    fix

--- packages/koa-to-json/test/jsonlize.spec.ts
+++ packages/koa-to-json/test/jsonlize.spec.ts
@@ -1,4 +1,4 @@
-import jsonlize from "./jsonlize";
+import jsonlize from "../lib/jsonlize";
 
 test("jsonlize", () => {
   expect(jsonlize({ x: 5, y: 6 })).toEqual({ x: 5, y: 6 });
@@ -11,6 +11,6 @@ test("jsonlize", () => {
     x: [10, null, null, null],
   });
   expect(jsonlize(new Date(2006, 0, 2, 15, 4, 5))).toEqual(
-    "2006-01-02T06:04:05.000Z"
+    new Date(2006, 0, 2, 15, 4, 5).toJSON()
   );
 });
