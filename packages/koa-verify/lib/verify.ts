f083bf6dd90558a3a5b12475644db55501ec72a5
Ara Park <siyual.bak@gmail.com>
2021-03-25 20:10:15 +0900

    feat: fix verify (#13)
    * re throw
    
    * remove rethrow

--- packages/koa-verify/lib/verify.ts
+++ packages/koa-verify/lib/verify.ts
@@ -1,5 +1,6 @@
 import Application from "koa";
 import { Position } from "koa-position";
+import { BadRequest } from "http-errors";
 
 function verify<T>(
   position: Position,
@@ -7,7 +8,14 @@ function verify<T>(
 ): Application.Middleware {
   return async (context, next) => {
     const value = await position.extract(context);
-    await validate(value as T);
+    try {
+      await validate(value as T);
+    } catch (e) {
+      if (typeof e.status === "number" || typeof e.statusCode === "number") {
+        throw e;
+      }
+      throw new BadRequest(e.message);
+    }
 
     await next();
   };
