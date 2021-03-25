ece20484a3289977375931bb4cb882f8a704e3f8
siyual-park <kdpark0723@gmail.com>
2021-03-25 15:21:54 +0900

    lint

--- packages/koa-to-json/lib/jsonlize.spec.ts
+++ packages/koa-to-json/lib/jsonlize.spec.ts
@@ -1,8 +1,16 @@
-import jsonlize from "../lib/jsonlize";
+import jsonlize from "./jsonlize";
 
 test("jsonlize", () => {
   expect(jsonlize({ x: 5, y: 6 })).toEqual({ x: 5, y: 6 });
-  expect(jsonlize([Number(3), String('false'), Boolean(false)])).toEqual([3, "false", false]);
-  expect(jsonlize({ x: [10, undefined, function(){}, Symbol('')] })).toEqual({ x: [10, null, null, null] });
-  expect(jsonlize(new Date(2006, 0, 2, 15, 4, 5))).toEqual("2006-01-02T06:04:05.000Z");
+  expect(jsonlize([Number(3), String("false"), Boolean(false)])).toEqual([
+    3,
+    "false",
+    false,
+  ]);
+  expect(jsonlize({ x: [10, undefined, function () {}, Symbol("")] })).toEqual({
+    x: [10, null, null, null],
+  });
+  expect(jsonlize(new Date(2006, 0, 2, 15, 4, 5))).toEqual(
+    "2006-01-02T06:04:05.000Z"
+  );
 });
