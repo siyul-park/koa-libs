787a7fdc8a6bd6eba9db06dcf42c49b8e5368c0c
siyual-park <kdpark0723@gmail.com>
2021-03-25 20:09:01 +0900

    remove rethrow

--- packages/koa-verify-joi/lib/by-schema.ts
+++ packages/koa-verify-joi/lib/by-schema.ts
@@ -1,15 +1,7 @@
 import { Schema } from "joi";
-import { BadRequest } from "http-errors";
 
-// eslint-disable-next-line  @typescript-eslint/no-explicit-any
-function bySchema<P = any>(schema: Schema<P>): (value: P) => Promise<void> {
-  return async (value) => {
-    try {
-      await schema.validateAsync(value);
-    } catch (e) {
-      throw new BadRequest(e.message);
-    }
-  };
+function bySchema<P>(schema: Schema<P>): (value: P) => Promise<void> {
+  return (value) => schema.validateAsync(value);
 }
 
 export default bySchema;
