48d76b551f58df9eaf21db692703ee5700bf0776
siyual-park <kdpark0723@gmail.com>
2021-03-25 19:51:18 +0900

    add error handling

--- packages/koa-deserialize/lib/deserialize.ts
+++ packages/koa-deserialize/lib/deserialize.ts
@@ -1,6 +1,7 @@
 import Application from "koa";
 import { Position } from "koa-position";
 import { Json } from "@course-design/types";
+import { BadRequest } from "http-errors";
 
 function deserialize<T>(
   position: Position,
@@ -8,8 +9,15 @@ function deserialize<T>(
 ): Application.Middleware {
   return async (context, next) => {
     const value = await position.extract(context);
-    await position.inject(context, await convert(value as Json));
 
+    let parsed: T;
+    try {
+      parsed = await convert(value as Json);
+    } catch (e) {
+      throw new BadRequest(e.message);
+    }
+
+    await position.inject(context, parsed);
     await next();
   };
 }
