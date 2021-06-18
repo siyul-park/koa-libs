effd893710b99c4ea42eaff0e6c887c6be8688c9
Ara Park <siyual.bak@gmail.com>
2021-06-18 13:10:12 +0900

    feat: fix contents type (#92)
    * fix serialize
    
    * change deserialize
    
    * change expose

--- packages/koa-deserialize/lib/deserialize.ts
+++ packages/koa-deserialize/lib/deserialize.ts
@@ -1,13 +1,17 @@
 import Application from "koa";
 import { Position } from "koa-position";
 import { Json } from "@course-design/types";
-import { BadRequest } from "http-errors";
+import { BadRequest, NotAcceptable } from "http-errors";
 
 function deserialize<T>(
   position: Position,
   convert: (value: Json) => T | Promise<T>
 ): Application.Middleware {
   return async (context, next) => {
+    if (context.request.type !== "application/json") {
+      throw new NotAcceptable("Content-Type must to be application/json");
+    }
+
     const value = (await position.extract(context)) as Json;
 
     let parsed: T;
