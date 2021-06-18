31b02b4b5b7f3e4303db8146e4848258ac7d74e2
Ara Park <siyual.bak@gmail.com>
2021-06-18 17:38:44 +0900

    feat: finally (#100)
    * add finalize
    
    * add await

---
+++ packages/koa-logic/lib/finalize.ts
@@ -0,0 +1,15 @@
+import { DefaultContext, DefaultState, Middleware } from "koa";
+
+function finalize<StateT = DefaultState, ContextT = DefaultContext>(
+  middleware: Middleware<StateT, ContextT>
+): Middleware<StateT, ContextT> {
+  return async (context, next) => {
+    try {
+      await next();
+    } finally {
+      await middleware(context, async () => {});
+    }
+  };
+}
+
+export default finalize;
