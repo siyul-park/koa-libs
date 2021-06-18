3d30726349b39a64da318458c4e264be8a76ea03
siyual-park <kdpark0723@gmail.com>
2021-06-18 17:22:40 +0900

    refactoring

--- packages/koa-logic/lib/multiplex.ts
+++ packages/koa-logic/lib/multiplex.ts
@@ -1,18 +1,16 @@
 import { DefaultContext, DefaultState, Middleware } from "koa";
 import Expression from "./expression";
 
+export type Matcher<ResultT> = (result: ResultT) => boolean | Promise<boolean>;
 function multiplex<ResultT, StateT = DefaultState, ContextT = DefaultContext>(
   expression: Expression<ResultT, StateT, ContextT>,
-  middlewares: [
-    (result: ResultT) => boolean | Promise<boolean>,
-    Middleware<StateT, ContextT>
-  ][]
+  middlewares: [Matcher<ResultT>, Middleware<StateT, ContextT>][]
 ): Middleware<StateT, ContextT> {
   return async (context, next) => {
     const value = await expression(context);
     // eslint-disable-next-line no-restricted-syntax
-    for await (const [match, middleware] of middlewares) {
-      const result = await match(value);
+    for await (const [matcher, middleware] of middlewares) {
+      const result = await matcher(value);
       if (result) {
         await middleware(context, next);
         return;
