81cc15fbd191d3d98ec68fc310e20a0a11d23718
siyual-park <kdpark0723@gmail.com>
2021-06-18 14:11:42 +0900

    feat: lint fix

--- packages/koa-logic/lib/not.ts
+++ packages/koa-logic/lib/not.ts
@@ -1,11 +1,13 @@
-import {DefaultContext, DefaultState} from "koa";
+import { DefaultContext, DefaultState } from "koa";
 import Expression from "./expression";
 
-function not<StateT = DefaultState, ContextT = DefaultContext>(expression: Expression<StateT, ContextT>): Expression<StateT, ContextT> {
+function not<StateT = DefaultState, ContextT = DefaultContext>(
+  expression: Expression<StateT, ContextT>
+): Expression<StateT, ContextT> {
   return async (context) => {
     const value = await expression(context);
     return !value;
-  }
+  };
 }
 
 export default not;
