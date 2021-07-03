252612cd11fa9563959ece8e074ffc0e71324ab0
siyual-park <kdpark0723@gmail.com>
2021-07-03 14:52:19 +0900

    feat: add context in serialize

--- packages/koa-serialize/lib/serialize.ts
+++ packages/koa-serialize/lib/serialize.ts
@@ -1,30 +1,44 @@
-import Application from "koa";
+import Application, {
+  DefaultContext,
+  DefaultState,
+  ParameterizedContext,
+} from "koa";
 import { Position } from "koa-position";
 
 import { Json } from "@course-design/types";
 import toJSON from "./to-json";
 
-export type SerializeOptions = {
-  serialize?: (value: unknown) => Json | Promise<Json>;
+export type SerializeOptions<
+  StateT = DefaultState,
+  ContextT = DefaultContext
+> = {
+  serialize?: (
+    value: unknown,
+    context: ParameterizedContext<StateT, ContextT>
+  ) => Json | Promise<Json>;
   replacer?: (key: string, value: unknown) => unknown | Promise<Json>;
 };
 
-function createDefaultSerialize(
-  options?: SerializeOptions
-): Required<SerializeOptions>["serialize"] {
-  return options?.replacer != null
-    ? (value: unknown) => toJSON(value, options?.replacer)
-    : toJSON;
+function createDefaultSerialize<
+  StateT = DefaultState,
+  ContextT = DefaultContext
+>(
+  options?: SerializeOptions<StateT, ContextT>
+): Required<SerializeOptions<StateT, ContextT>>["serialize"] {
+  if (options?.replacer != null) {
+    return (value: unknown) => toJSON(value, options?.replacer);
+  }
+  return (value: unknown) => toJSON(value);
 }
 
-function serialize(
+function serialize<StateT = DefaultState, ContextT = DefaultContext>(
   position: Position,
-  options?: SerializeOptions
-): Application.Middleware {
+  options?: SerializeOptions<StateT, ContextT>
+): Application.Middleware<StateT, ContextT> {
   const finalSerialize = options?.serialize ?? createDefaultSerialize(options);
   return async (context, next) => {
     const value = await position.extract(context);
-    await position.inject(context, await finalSerialize(value));
+    await position.inject(context, await finalSerialize(value, context));
 
     await next();
   };
