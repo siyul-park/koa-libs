bfc6e0fb5a4ebeb5684a38da994ac5fa1d16c644
siyual-park <kdpark0723@gmail.com>
2021-07-11 20:34:28 +0900

    remove async in to Json

--- packages/koa-serialize/lib/as-json.ts
+++ packages/koa-serialize/lib/as-json.ts
@@ -1,10 +1,11 @@
 import { DefaultContext, DefaultState } from "koa";
 import { Json } from "@course-design/types";
+
 import Serializer from "./serializer";
 import toJSON from "./to-json";
 
 function asJson<StateT = DefaultState, ContextT = DefaultContext>(
-  replacer?: (key: string, value: unknown) => unknown | Promise<unknown>
+  replacer?: (key: string, value: unknown) => unknown
 ): Serializer<Json, unknown, StateT, ContextT> {
   return (value) => toJSON(value, replacer);
 }
